5a74bb01b6e89ad789518abf28dbe68d3e377e49
yiqing <yiqing_95@qq.com>
2017-09-09 22:31:28 +0800

    添加readme文件

---
+++ src/hello-go/gowebexamples/READEME.md
@@ -0,0 +1,3 @@
+学习go的web编程基础
+
+手动敲[gowebexamples](https://gowebexamples.com) 所示出的代码
--- src/hello-go/gowebexamples/main.go
+++ src/hello-go/gowebexamples/main.go
@@ -2,9 +2,13 @@ package main
 
 import "net/http"
 
+import "fmt"
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello web"))
+		w.Write([]byte("hello web\n"))
+
+		fmt.Fprintf(w, "Hello you 'v requested : %s\n", r.URL.Path)
 	})
 
 	http.ListenAndServe(":80", nil)
